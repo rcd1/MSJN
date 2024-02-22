3c10e801cf81d39c24dc191e955c5942add310c7
Joy-lj <joyluya@gmail.com>
2024-02-22 14:16:13 -0500

    adding Administrator stub

---
+++ Administrator.java
@@ -0,0 +1,28 @@
+public class Administrator {
+    private ArrayList<Student> students;
+    private ArrayList<Advisor> advisors;
+    
+    public Administrator (String firstName, String lastName, String email) {
+
+    }
+
+    private addCourse(Course course) {
+
+    }
+
+    private removeCourse(Course course) {
+
+    }
+
+    private addStudentCourse(Student student, Course course) {
+
+    }
+
+    private addStudentGrade(Student student, Course course, Grade grade) {
+
+    }
+
+    private updateStudentGrade(Student student, Course course, Grade grade) {
+        
+    }
+}
