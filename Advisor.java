119ce33e51da7212d39badd3cbad787f9d5136fc
unknown <sj49@email.sc.edu>
2024-02-22 14:15:05 -0500

    Stub my toe:)

---
+++ Advisor.java
@@ -0,0 +1,21 @@
+public class Advisor {
+    private ArrayList<Student> students{
+
+    }
+
+    public Advisor(String firstName, String lastName, String email){
+
+    }
+    public boolean addStudent(Student student){
+
+    }
+    public boolean removeStudent(Student student){
+
+    }
+    public void addNote(Student student, String note){
+
+    }
+    public void addAdvisor(Student student){
+        
+    }
+}
---
+++ Major.java
@@ -0,0 +1,23 @@
+public class Major {
+ private UUID majorid;
+ private String majorName;
+ private RequirementSet requiredCourses;
+ private ApplicationI DapplicationID;
+}
+
+public major(){
+
+}
+
+protected void addCourse(Course course){
+
+}
+
+protected void removeCourse(Course course){
+
+}
+protected boolean changeApplicationID(int choice){
+    
+}
+
+
