c3bffa2744ee4adf7fa1195636449873bd4c8fce
Joy-lj <joyluya@gmail.com>
2024-02-25 18:21:23 -0500

    added ApplicationUI stub

---
+++ ApplicationUI.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ApplicationUI {
+    private Scanner scanner;
+    private DegreeFacade application;
+    
+    public void run() {
+
+    }
+
+    private void displayMainMenu() {
+
+    }
+
+    private int getUserOption() {
+        
+    }
+}
