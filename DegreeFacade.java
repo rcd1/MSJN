c725640a226a8cdb4cfdc3224abac0ad3af28728
Joy-lj <joyluya@gmail.com>
2024-02-25 18:15:52 -0500

    added DegreeFacade stub

---
+++ DegreeFacade.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class DegreeFacade {
+    private UserList userList;
+    private User user;
+    private CourseList courseList;
+    private MajorList majorList;
+
+    public DegreeFacade() {
+
+    }
+
+    public User login(String userName, String password) [
+
+    ]
+
+    public void logout() {
+
+    }
+
+    public void addCourse() {
+
+    }
+
+    public void removeCourse() {
+
+    }
+
+    public User createAccount(String firstName, String lastName, String email, String password) {
+
+    }
+
+    public ArrayList<Course> getCourses() {
+
+    }
+
+    public ArrayList<Course> findCourses() {
+
+    }
+
+    public ArrayList<Course> findCourses(String keyword) {
+        
+    }
+}
+
