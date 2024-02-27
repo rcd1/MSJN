9a075808736b831868131ac9e801688dfb004fbb
unknown <sj49@email.sc.edu>
2024-02-27 14:24:57 -0500

    Doing Comments

--- Course.java
+++ Course.java
@@ -1,5 +1,8 @@
 import java.util.UUID;
 import java.util.ArrayList;
+/*
+ * The attributes of the course class 
+ */
 public class Course {
     private UUID courseID;
     private Designator designator;
@@ -9,7 +12,16 @@ public class Course {
     private ArrayList<Keyword> keywords;
     private int preferredSemester;
 
-    
+    /**
+     * A Mutator method for the parameters setted up in the Course class 
+     * @param courseID 
+     * @param designator The type of course the student will be taking (e.g. CHEM, CSCE, MATH, etc)
+     * @param number digits meant tp imply the level of the course 
+     * @param hours The number of hours the student will be taking
+     * @param requirements the requistes that needs to be completed before or during taking the course
+     * @param keywords The Carolina Core and Honors
+     * @param preferredSemester The semester that course is recommended by the major map
+     */
     public Course(UUID courseID, Designator designator, String number, int hours, ArrayList<Requirement> requirements,
             ArrayList<Keyword> keywords, int preferredSemester) {
         this.courseID = courseID;
@@ -76,17 +88,26 @@ public class Course {
         return keywords;
     }
 
-
+    /**
+     * A mutator method for the user class
+     * @param keywords The code of the courses such as the Carolina Cores and Honors Courses
+     */
     public void setKeywords(ArrayList<Keyword> keywords) {
         this.keywords = keywords;
     }
 
-
+    /**
+     * An accessor that grabs the preferred semester
+     * @return the preferred semester that course should be taken in based on the major maps
+     */
     public int getPreferredSemester() {
         return preferredSemester;
     }
 
-
+    /**
+     * A mutator method for the user's preferred semester
+     * @param preferredSemester the preferred semester that course should be taken in based on the major maps
+     */
     public void setPreferredSemester(int preferredSemester) {
         this.preferredSemester = preferredSemester;
     }
