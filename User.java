d0cde0569e359c0d4f24fcc359dfde2e0fd9f182
unknown <sj49@email.sc.edu>
2024-02-29 14:01:11 -0500

    commenting

--- User.java
+++ User.java
@@ -1,9 +1,20 @@
 import java.util.UUID;
-
+/**
+ * The class that the UserList will go to find the student and advisor loginning
+ * Also the clas the student and advisor will be inheriting
+ */
 public abstract class User {
     protected String firstName, lastName, email, password;
     protected UUID userID;
 
+    /**
+     * 
+     * @param firstName The first name of user 
+     * @param lastName The last name of user
+     * @param email The email of user 
+     * @param password The password of user
+     * @param userID The ID for the user 
+     */
     public User(String firstName, String lastName, String email, String password, UUID userID) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -12,10 +23,18 @@ public abstract class User {
         this.userID = userID;
     }
 
+    /**
+     * An accessor method for the user's email
+     * @return the email of the user
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * An accessor method of the user's password 
+     * @return the password of the user
+     */
     public String getPassword() {
         return password;
     }
