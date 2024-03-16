c8444423b57e649de4b4d90570d1cc0091131d55
Mabel H <mahurd@email.sc.edu>
2024-03-16 10:59:35 -0400

    Added titles to some keywords

--- Keyword.java
+++ Keyword.java
@@ -18,17 +18,35 @@ public enum Keyword {
     LSE, 
     
     // Everything past this point is a hacky solution used for "this or that" classes inside a major map
-    COP1, // Chem or Physics 1 
-    COP2, // Chem or Physics 2
-    SPCHC, // SPCH 140 or SPCH 145 or SPCH 230
-    ENGLW, // Technical writing or business writing 
+    COP1("CHEM 111 or PHYS 111"), // Chem or Physics 1 
+    COP2("CHEM 112 or PHYS 112"), // Chem or Physics 2
+    SPCHC("SPCH 140 or SPCH 145 or SPCH 230"), // SPCH 140 or SPCH 145 or SPCH 230
+    ENGLW ("ENGL 462 or ENGL 463"), // Technical writing or business writing 
 
-    AP0,
-    AP1,
-    AP2,
-    AP3,
-    AP4,
-    AP5,
-    CSCEME;
+    AP0(ApplicationID.UNDECLARED),
+    AP1(ApplicationID.SCIENCE),
+    AP2(ApplicationID.MATH),
+    AP3(ApplicationID.DIGITALDESIGN),
+    AP4(ApplicationID.ROBOTICS),
+    AP5(ApplicationID.SPEECH),
+    CSCEME("CSCE Major Elective");
+
+    private Keyword() {
+        title = "none";
+    }
+
+    private Keyword(String title) {
+        this.title = title;
+    }
+
+    private Keyword(ApplicationID appid) {
+        this.title = appid.getName();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    String title;
 
 }
