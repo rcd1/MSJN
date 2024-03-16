75f1ef513aef6f5031ad14a37a973b3b73b2370c
rcd1 <rcd1@email.sc.edu>
2024-03-15 20:18:12 -0400

    Updating dataWriterTest to let me test writing data to majors.json

--- dataWriterTest.java
+++ dataWriterTest.java
@@ -4,13 +4,20 @@ import java.util.UUID;
 
 public class dataWriterTest {
     private Scanner input;
+    private MajorList test;
+    private CourseList courseList;
+    private UserList userList;
 
     public dataWriterTest() {
-        input = new Scanner(System.in);
+        this.input = new Scanner(System.in);
+        this.test = MajorList.getInstance();
+        this.courseList = CourseList.getInstance();
+        this.userList = UserList.getInstance();
     }
+
     public static void main(String[] args) {
-       dataWriterTest hi = new dataWriterTest();
-       hi.usersTest();
+        dataWriterTest hi = new dataWriterTest();
+        hi.majorTest();
     }
 
     public void majorTest() {
@@ -19,36 +26,139 @@ public class dataWriterTest {
         CourseList courseList = CourseList.getInstance();
         ArrayList<Course> courses = courseList.getCourses();
         ArrayList<Major> majors = test.getMajors();
-        for (Major major : majors) {
+        System.out.println("Majors Available:");
+        for (int i = 1; i < majors.size(); i++) {
+            System.out.println(i + ". " + majors.get(i).getMajorName());
+        }
+        System.out.println("Pick a number for the Major you would like to work on: ");
+        int userInt = input.nextInt();
+        input.nextLine();
+        Major toWorkOn = majors.get(userInt);
+        System.out.println("What would you like change in " + toWorkOn.getMajorName() + "?");
+        displayMajorRevisionOptions();
+        userInt = input.nextInt();
+        input.nextLine();
+
+        switch (userInt) {
+            case 1:
+                workOnSemesterPlans(toWorkOn);
+                break;
+            case 2:
+                workOnMajorRequirements(toWorkOn);
+                break;
+            default:
+                System.exit(0);
+        }
+
+        input.close();
+        test.logout();
+    }
+
+    private void workOnMajorRequirements(Major toWorkOn) {
+        ArrayList<MajorRequirement> majorRequirements = toWorkOn.getMajorRequirements();
+        boolean keepAdding = true;
+        while (keepAdding) {
             System.out.println("Title?: ");
             String title = input.nextLine();
             System.out.println("MinHours?: ");
             int minHours = input.nextInt();
+            input.nextLine();
             System.out.println("MaxHours?: ");
             int maxHours = input.nextInt();
             input.nextLine();
-            System.out.println("RequirementSet Title?: ");
-            String reqTitle = input.nextLine();
+            ArrayList<RequirementSet> toAdd = fillRequirementSets();
+            majorRequirements.add(new MajorRequirement(title, minHours, maxHours, toAdd));
+            System.out.println("Keep Adding? y/n: ");
+            String choice = input.nextLine().toLowerCase();
+            switch (choice) {
+                case "y":
+                    continue;
+                default:
+                    keepAdding = false;
+                    break;
+            }
+        }
+
+    }
+
+    private ArrayList<RequirementSet> fillRequirementSets() {
+        boolean keepAdding = true;
+        ArrayList<RequirementSet> reqSets = new ArrayList<>();
+
+        while (keepAdding) {
             System.out.println("Minimum Grade?: ");
             String reqGrade = input.nextLine().toUpperCase();
             Grade grade = Grade.valueOf(reqGrade);
-            ArrayList<Course> acceptableCourses = new ArrayList<>();
-            acceptableCourses.add(courseList.getCourse("MATH", "142"));
-            acceptableCourses.add(courseList.getCourse("CSCE", "330"));
-            ArrayList<RequirementSet> reqSets = new ArrayList<>();
-            reqSets.add(new AndRequirement(reqTitle, acceptableCourses, grade));
-            major.addMajorRequirement(new MajorRequirement(reqTitle, minHours, maxHours, reqSets));
+            ArrayList<Course> acceptableCourses = fillCourseArray();
+            System.out.println("Is this an And or Or RequirementSet? (a/o)?: ");
+            String reqType = input.nextLine();
+            switch (reqType) {
+                case "a":
+                    reqSets.add(new AndRequirement(acceptableCourses, grade));
+                    break;
+
+                case "o":
+                    reqSets.add(new OrRequirement(acceptableCourses, grade));
+                    break;
+                default:
+                    System.out.println("ERROR: BAD CHARACTER, FORSHAME");
+                    System.exit(0);
+                    break;
+            }
+            System.out.println("Keep Adding? y/n: ");
+            String choice = input.nextLine().toLowerCase();
+            switch (choice) {
+                case "y":
+                    continue;
+                default:
+                    keepAdding = false;
+                    break;
+            }
         }
-        System.out.println("Done");
-        input.close();
-        test.logout();
+        return reqSets;
+    }
+
+    private void workOnSemesterPlans(Major toWorkOn) {
+        ArrayList<SemesterPlan> recommendedSemesterPlans = toWorkOn.getRecommendedSemesterPlans();
+        while (recommendedSemesterPlans.size() < 8) {
+            ArrayList<Course> toAdd = fillCourseArray();
+            recommendedSemesterPlans.add(new SemesterPlan(toAdd));
+        }
+
+    }
+
+    private ArrayList<Course> fillCourseArray() {
+        ArrayList<Course> toAdd = new ArrayList<>();
+        boolean keepAdding = true;
+        while (keepAdding) {
+            System.out.println(
+                    "What course would you like to add? (Write the 4 letter code and the full number ex. CSCE 146, CHEM 111L, etc.)");
+            String courseString = input.nextLine();
+            String[] values = courseString.split(" ");
+            toAdd.add(courseList.getCourse(values[0], values[1]));
+            System.out.println("Keep Adding? y/n: ");
+            String choice = input.nextLine().toLowerCase();
+            switch (choice) {
+                case "y":
+                    continue;
+                default:
+                    keepAdding = false;
+                    break;
+            }
+        }
+        return toAdd;
+    }
+
+    private void displayMajorRevisionOptions() {
+        System.out.println("1. Semester Plans");
+        System.out.println("2. Major Requirements");
     }
 
     public void usersTest() {
         UserList userList = UserList.getInstance();
         ArrayList<Student> students = userList.getStudents();
         ArrayList<Advisor> advisors = userList.getAdvisors();
-        Student student = new Student( "Test", "Terry", "email@email.email", "passwordbutcool", UUID.randomUUID());
+        Student student = new Student("Test", "Terry", "email@email.email", "passwordbutcool", UUID.randomUUID());
         students.add(student);
         Advisor advisor = new Advisor("Testing", "Testerson", "The third", "Esquire", UUID.randomUUID());
         advisors.add(advisor);
