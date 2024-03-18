0b4a67dcc6f66fd6ed6f6048d1775457218f9ad2
Mabel H <mahurd@email.sc.edu>
2024-03-17 21:48:27 -0400

    Finishing touches

--- ApplicationUI.java
+++ ApplicationUI.java
@@ -180,6 +180,7 @@ public class ApplicationUI {
             clear();
             switch (option) {
                 case "view student profile":
+                    System.out.println(selectedStudent.getApplicationID().getName());
                     selectedStudent.displaySemesterPlan();
                     // application.viewStudentProgress(selectedStudent);
                     break;
@@ -251,14 +252,14 @@ public class ApplicationUI {
         System.out.println("Logged in as " + user.getFirstName() + " " + user.getLastName());
         if (user instanceof Advisor) {
             System.out.println("Manage Your Students");
+            System.out.println("View Semester Plan");
+            System.out.println("Save Semester Plan to Text File");
         } else if (user instanceof Student) {
             System.out.println("View your progress");
             System.out.println("Choose Application Area");
             System.out.println("View Courses");
             System.out.println("Fill Requirement");
         }
-        System.out.println("View Semester Plan");
-        System.out.println("Save Semester Plan to Text File");
         System.out.println("Logout");
         System.out.print("Enter your choice: ");
     }
