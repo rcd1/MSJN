0c3b456b008b04c9f1945df3259d195336d6a12e
unknown <sj49@email.sc.edu>
2024-03-24 22:56:18 -0400

    testing MajorTest

---
+++ testFiles/MajorTest.java
@@ -0,0 +1,49 @@
+package testFiles;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.UUID;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import msjn.*;
+
+class MajorTest{
+    private Major major;
+    private UUID majorId;
+    private String majorName;
+    private ArrayList<MajorRequirement> majorRequirements = new ArrayList<>();
+    private ArrayList<SemesterPlan> recommendedSemesterPlans = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+
+        UUID majorID = UUID.randomUUID();
+        String majorName = "Computer Science";
+        majorRequirements = new ArrayList<>();
+        recommendedSemesterPlans = new ArrayList<>();
+        major = new Major(majorId, majorName, majorRequirements, recommendedSemesterPlans);
+    }
+
+    @Test
+    public void testConstruction() {
+        assertEquals(majorId, major.getMajorid());
+        assertEquals(majorName, major.getMajorName());
+        assertEquals(majorRequirements, major.getMajorRequirements());
+        assertEquals(recommendedSemesterPlans, major.getRecommendedSemesterPlans());
+    }
+
+    @Test //Succesfull
+    public void testCheckMajors() {
+        assertTrue(major.checkMajor("Computer Science"));
+        assertFalse(major.checkMajor("Blah"));
+    }
+    
+    @Test //Successfull
+    public void testAddMajorRequirement() {
+        MajorRequirement test = new MajorRequirement(majorName, 0, 0, null);
+        major.addMajorRequirement(test);
+        assertTrue(major.getMajorRequirements().contains(test));
+    }
+}
